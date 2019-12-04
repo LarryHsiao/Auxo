92218b51b8fdff08c75f0398081182e5684c4898
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-12-04 12:12:51 +0800

    File searching in file list

--- src/main/java/com/larryhsiao/auxo/controller/FileList.java
+++ src/main/java/com/larryhsiao/auxo/controller/FileList.java
@@ -4,6 +4,8 @@ import com.larryhsiao.auxo.tagging.FileByName;
 import com.larryhsiao.auxo.tagging.TagDbConn;
 import com.larryhsiao.auxo.workspace.FsFiles;
 import com.silverhetch.clotho.database.SingleConn;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,11 +13,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static javafx.scene.input.MouseButton.PRIMARY;
 
@@ -24,11 +32,19 @@ import static javafx.scene.input.MouseButton.PRIMARY;
  */
 public class FileList implements Initializable {
     private final ObservableList<String> data = FXCollections.observableArrayList();
-    @FXML
-    private ListView<String> fileList;
+    @FXML private TextField searchInput;
+    @FXML private ListView<String> fileList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        searchInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            data.clear();
+            data.addAll(
+                new FsFiles().value().entrySet().stream()
+                    .filter(entry -> entry.getKey().contains(searchInput.textProperty().getValue()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)).keySet()
+            );
+        });
         data.addAll(new FsFiles().value().keySet());
         fileList.setItems(data);
         fileList.setOnMouseClicked(event -> {
